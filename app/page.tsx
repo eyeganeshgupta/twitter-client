fc47a8acf0314f36f9e524cb53304dd25b0f3a77
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-12 19:50:54 +0530

    FeedCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
+import FeedCard from "@/components/FeedCard";
+import { SlOptions } from "react-icons/sl";
 
 interface TwitterSidebarButton {
   title: string;
@@ -52,7 +54,19 @@ export default function Home() {
           </div>
 
           <div className="mt-1 text-xl font-semibold pr-4">
-            <ul></ul>
+            <ul>
+              {sidebarMenuItems.map((item) => {
+                return (
+                  <li
+                    key={item.title}
+                    className="flex justify-start items-center gap-4 mt-2 hover:bg-gray-800 rounded-full px-3 py-3 w-fit cursor-pointer"
+                  >
+                    <span className="text-3xl">{item.icon}</span>
+                    <span>{item.title}</span>
+                  </li>
+                );
+              })}
+            </ul>
 
             <div className="mt-5 px-3">
               <button className="bg-[#1d9bf0] text-lg font-semibold py-2 px-4 w-full rounded-full">
@@ -61,7 +75,16 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600"></div>
+        <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll border-gray-600">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
         <div className="col-span-3"></div>
       </div>
     </div>
